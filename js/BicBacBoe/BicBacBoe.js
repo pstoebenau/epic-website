43b02708cd116576f5cf98477460c5d6972387a8
Patrick Star <patx5x5x4@gmail.com>
2018-05-21 01:00:00 -0400

    Initing

---
+++ js/BicBacBoe/BicBacBoe.js
@@ -0,0 +1,198 @@
+let menuBar = document.getElementById("menuBar");
+let menuBarHeight = menuBar.clientHeight + 3;
+let dimensionSlider = document.getElementById("dimensionRange");
+let zoomSlider = document.getElementById("zoomRange");
+let canvas = document.querySelector('canvas');
+canvas.addEventListener("mousemove", updateMousePos);
+canvas.addEventListener("mouseup", stopSelect);
+dimensionSlider.addEventListener("input", restartBoard);
+zoomSlider.addEventListener("input", zoom);
+canvas.addEventListener("mousedown", startSelect);
+window.addEventListener("resize", resizeCanvas);
+
+let c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - menuBarHeight;
+
+let mouse = {x:0, y:0};
+let isDragging = false;
+let startMouse = {x:0, y:0};
+let startBoardPos = {x:0, y:0};
+let fps = 30;
+
+let board = new ticTacToeBoard(canvas.width/2, canvas.height/2, dimensionSlider.value);
+if(canvas.width < canvas.height){
+  board.size = canvas.width/2;
+}else{
+  board.size = canvas.height/2;
+}
+board.initialize();
+
+function restartBoard() {
+  board.restart();
+}
+
+function resizeCanvas(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - menuBarHeight;
+  let boardSize;
+  if(canvas.width < canvas.height){
+    boardSize = canvas.width/2;
+  }else{
+    boardSize = canvas.height/2;
+  }
+  board.resize(boardSize);
+  board.move(canvas.width/2, canvas.height/2);
+}
+
+function zoom(){
+  board.resize(board.size*zoomSlider.value/10);
+}
+
+function position(x, y){
+  this.x = x;
+  this.y = y;
+}
+
+function startSelect(){
+  mouse.x = event.clientX;
+  mouse.y = event.clientY-menuBarHeight;
+  let boxSize = board.grids[board.dimensions][0].size/3;
+  for(let i = 0; i < board.grids[board.dimensions].length; i++){
+    if(!board.grids[board.dimensions][i].selectable || board.grids[board.dimensions][i].closed){
+      continue;
+    }
+    for (let j = 0; j < board.grids[board.dimensions][i].playPoints.length; j++)
+      for (let k = 0; k < board.grids[board.dimensions][i].playPoints[j].length; k++)
+        if(board.grids[board.dimensions][i].playPoints[j][k].x > mouse.x-boxSize/2
+           && board.grids[board.dimensions][i].playPoints[j][k].x < mouse.x+boxSize/2
+           && board.grids[board.dimensions][i].playPoints[j][k].y > mouse.y-boxSize/2
+           && board.grids[board.dimensions][i].playPoints[j][k].y < mouse.y+boxSize/2){
+             return;
+        }
+  }
+
+  if(isDragging == false){
+    startMouse.x = mouse.x;
+    startMouse.y = mouse.y;
+    startBoardPos.x = board.position.x;
+    startBoardPos.y = board.position.y;
+  }
+  isDragging = true;
+}
+
+function updateMousePos(){
+  mouse.x = event.clientX;
+  mouse.y = event.clientY-menuBarHeight;
+  if(isDragging){
+    let posX = startBoardPos.x + (mouse.x-startMouse.x);
+    let posY = startBoardPos.y + (mouse.y-startMouse.y);
+    board.move(posX, posY);
+  }
+}
+function stopSelect(){
+  if(isDragging){
+    isDragging = false;
+    return;
+  }
+
+  let grids = board.grids[board.dimensions];
+  let boxSize = grids[0].size/3;
+  if(board.dimensions == 0){
+    for (let j = 0; j < grids[0].playPoints.length; j++)
+      for (let k = 0; k < grids[0].playPoints[j].length; k++)
+        if(grids[0].playPoints[j][k].x > mouse.x-boxSize/2
+           && grids[0].playPoints[j][k].x < mouse.x+boxSize/2
+           && grids[0].playPoints[j][k].y > mouse.y-boxSize/2
+           && grids[0].playPoints[j][k].y < mouse.y+boxSize/2){
+           if(grids[0].selectable && !grids[0].moves[j][k]){
+             grids[0].fillBox(j, k, board.turn);
+             board.turn = 1-board.turn;
+             checkForWin(grids[0], 0);
+           }
+        }
+  }else{
+    selectSelectable(grids, boxSize);
+  }
+}
+
+function selectSelectable(grids, boxSize){
+  for(let i = 0; i < grids.length; i++)
+    for (let j = 0; j < grids[i].playPoints.length; j++)
+      for (let k = 0; k < grids[i].playPoints[j].length; k++)
+        if(grids[i].playPoints[j][k].x > mouse.x-boxSize/2 && grids[i].playPoints[j][k].x < mouse.x+boxSize/2
+           && grids[i].playPoints[j][k].y > mouse.y-boxSize/2 && grids[i].playPoints[j][k].y < mouse.y+boxSize/2)
+           if(grids[(i-i%9)+(j*3+k)].closed){
+             makeAllSelectable(grids, i-i%9);
+             grids[i].fillBox(j, k, board.turn);
+             checkForWin(grids[i], board.dimensions, i,(j*3+k));
+             board.turn = 1-board.turn
+             return;
+           }else if(grids[i].selectable && !grids[i].closed && !grids[i].moves[j][k]){
+            resetSelectable(grids);
+            grids[i].selectable = false;
+            grids[(i-i%9)+(j*3+k)].selectable = true;
+            grids[i].fillBox(j, k, board.turn);
+            checkForWin(grids[i], board.dimensions, i, (j*3+k));
+            board.turn = 1-board.turn
+            return;
+          }
+}
+
+function checkForWin(grid, dimension, i, nextIndex){
+  let winner = grid.checkWin();
+  let index = Math.floor(i/9);
+  let row = Math.floor((i%9)/3);
+  let col = (i%9)-row*3;
+
+  if(dimension == 0){
+    if(winner != 0){
+      alert("Player " + winner + " has won! THE GAME IS OVER!");
+      for(let i = 0; i < board.grids.length; i++)
+        resetSelectable(board.grids[i]);
+    }else if(board.dimensions != 0){
+      board.grids[board.dimensions][(i-i%9)+nextIndex].selectable = true;
+    }
+    return;
+  }
+
+  if(winner != 0){
+    grid.closed = true;
+    board.grids[dimension-1][index].fillBox(row, col, board.turn);
+    checkForWin(board.grids[dimension-1][index], dimension-1, index, nextIndex+9*(row*3+col));
+  }else{
+    board.grids[board.dimensions][(i-i%9)+nextIndex].selectable = true;
+  }
+}
+
+function resetSelectable(grids){
+  for(let i = 0; i < grids.length; i++)
+    grids[i].selectable = false;
+}
+
+function makeAllSelectable(grids, index){
+  for(let i = index; i < index+9; i++)
+    grids[i].selectable = true;
+}
+
+let frameRate = 1000/fps;
+let elapsed = 0;
+let past = Date.now();
+function setFrameRate(){
+  requestAnimationFrame(setFrameRate);
+
+  elapsed = Date.now() - past;
+
+  if(elapsed >= frameRate){
+    past = Date.now()- (elapsed%(frameRate));
+    animate();
+  }
+}
+
+function animate() {
+  c.clearRect(0,0,canvas.width,canvas.height);
+  board.update();
+}
+
+setFrameRate();
+resizeCanvas();
---
+++ js/BicBacBoe/board.js
@@ -0,0 +1,210 @@
+function ticTacToeBoard(x, y, dimensions){
+  this.position = new position(0,0);
+  this.position.x = x;
+  this.position.y = y;
+  this.size;
+  this.dimensions = dimensions;
+  this.turn = 0;
+  this.grids = [];
+
+  this.initialize = function(){
+    this.grids[0] = [];
+    this.grids[0][0] = new grid(this.position.x, this.position.y, this.size);
+    if(this.dimensions == 0)
+      this.grids[0][0].selectable = true;
+    this.createGrids(this.grids[0][0], this.dimensions);
+  }
+
+  this.restart = function(){
+    this.position.x = canvas.width/2;
+    this.position.y = canvas.height/2;
+    this.dimensions = dimensionSlider.value;
+    this.grids = [];
+    this.turn = 0;
+    this.initialize();
+  }
+
+  this.resize = function(size){
+    this.grids[0][0].size = size;
+    this.grids[0][0].updatePlayPoints();
+    for(let i = 1; i < this.grids.length; i++)
+      for(let j = 0; j < this.grids[i].length; j++){
+        this.grids[i][j].size = size/(3.5**i);
+        let row = Math.floor((j%9)/3);
+        let col = (j%9)-row*3;
+        this.grids[i][j].position.x = this.grids[i-1][Math.floor(j/9)].playPoints[row][col].x;
+        this.grids[i][j].position.y = this.grids[i-1][Math.floor(j/9)].playPoints[row][col].y;
+        this.grids[i][j].updatePlayPoints();
+      }
+  }
+
+  this.move = function(posX, posY){
+    let movDistX = posX - this.position.x;
+    let movDistY = posY - this.position.y;
+    this.position = new position(posX, posY);
+    for(let i = 0; i < this.grids.length; i++)
+      for(let j = 0; j < this.grids[i].length; j++){
+        this.grids[i][j].position.x += movDistX;
+        this.grids[i][j].position.y += movDistY;
+        this.grids[i][j].updatePlayPoints();
+      }
+  }
+
+  this.draw = function(){
+    for (let i = 0; i < this.grids.length; i++) {
+      for (let j = 0; j < this.grids[i].length; j++) {
+        this.grids[i][j].draw();
+      }
+    }
+  }
+
+  this.createGrids = function(pastGrid, dimensions){
+    if(dimensions == 0)
+      return;
+
+    let dim = this.dimensions-dimensions+1;
+    if(this.grids[dim] == null)
+      this.grids[dim] = [];
+    for(let i = 0; i < pastGrid.playPoints.length; i++){
+      for (let j = 0; j < pastGrid.playPoints[i].length; j++) {
+        let currentGrid = new grid(pastGrid.playPoints[i][j].x, pastGrid.playPoints[i][j].y, pastGrid.size/3.5);
+        if(dim == this.dimensions)
+          currentGrid.selectable = true;
+        this.grids[dim].push(currentGrid);
+        if(dimensions > 1)
+          this.createGrids(currentGrid, dimensions-1);
+      }
+    }
+  }
+
+  this.update = function(){
+    this.draw();
+  }
+}
+
+function grid(x, y, size){
+  this.position = new position(x, y);
+  this.closed = false;
+  this.winner = 0;
+  this.selectable = false;
+  this.size = size;
+  this.playPoints = [];
+  this.playPoints[0] = [];
+  this.playPoints[1] = [];
+  this.playPoints[2] = [];
+  this.moves = [];
+  this.moves[0] = [];
+  this.moves[1] = [];
+  this.moves[2] = [];
+
+  this.updatePlayPoints = function(){
+    this.playPoints[0][0] = new position(this.position.x-this.size/3, this.position.y-this.size/3);
+    this.playPoints[0][1] = new position(this.position.x, this.position.y-this.size/3);
+    this.playPoints[0][2] = new position(this.position.x+this.size/3, this.position.y-this.size/3);
+    this.playPoints[1][0] = new position(this.position.x-this.size/3, this.position.y);
+    this.playPoints[1][1] = new position(this.position.x, this.position.y);
+    this.playPoints[1][2] = new position(this.position.x+this.size/3, this.position.y);
+    this.playPoints[2][0] = new position(this.position.x-this.size/3, this.position.y+this.size/3);
+    this.playPoints[2][1] = new position(this.position.x, this.position.y+this.size/3);
+    this.playPoints[2][2] = new position(this.position.x+this.size/3, this.position.y+this.size/3);
+  }
+
+  this.draw = function(){
+    //Grid
+    c.beginPath();
+    c.moveTo(this.position.x-this.size/6, this.position.y-this.size/2);
+    c.lineTo(this.position.x-this.size/6, this.position.y+this.size/2);
+    c.stroke();
+    c.closePath();
+    c.beginPath();
+    c.moveTo(this.position.x+this.size/6, this.position.y-this.size/2);
+    c.lineTo(this.position.x+this.size/6, this.position.y+this.size/2);
+    c.stroke();
+    c.closePath();
+    c.beginPath();
+    c.moveTo(this.position.x-this.size/2, this.position.y-this.size/6);
+    c.lineTo(this.position.x+this.size/2, this.position.y-this.size/6);
+    c.stroke();
+    c.closePath();
+    c.beginPath();
+    c.moveTo(this.position.x-this.size/2, this.position.y+this.size/6);
+    c.lineTo(this.position.x+this.size/2, this.position.y+this.size/6);
+    c.stroke();
+    c.closePath();
+
+    //Player markers
+    for (let i = 0; i < this.moves.length; i++) {
+      for (let j = 0; j < this.moves[i].length; j++) {
+        if(this.moves[i][j] == "X" || this.moves[i][j] == "O"){
+          c.beginPath();
+          c.font = this.size/3 + "px Arial";
+          c.fillStyle = "#000";
+          c.textBaseline = "middle";
+          c.textAlign = "center";
+          c.fillText(this.moves[i][j], this.playPoints[i][j].x, this.playPoints[i][j].y);
+          c.closePath();
+        }
+      }
+    }
+
+    //Selectable highlight
+    if(this.selectable && !this.closed){
+      c.beginPath();
+      c.globalAlpha = 0.2;
+      c.rect(this.position.x-this.size/2,this.position.y-this.size/2,this.size,this.size);
+      c.fillStyle = "green";
+      c.fill();
+      c.globalAlpha = 1;
+      c.closePath();
+    }
+  }
+
+  this.fillBox = function(row, col, player){
+
+    if(this.moves[row][col] == "X" || this.moves[row][col] == "O")
+      return;
+    if(player == 0)
+      this.moves[row][col] = "X";
+    else if(player == 1)
+      this.moves[row][col] = "O";
+    else
+      console.error("Invalid box index!");
+  }
+
+  this.checkWin = function(){
+    let playerMark = ["X", "O"];
+
+    for(let i = 0; i < 2; i++){
+      //Check rows
+      for(let j = 0; j < 3; j++){
+        if(this.trailCheck(j, 0, 0, 1, playerMark[i]))
+          return i+1;
+      }
+
+      //Check columns
+      for(let j = 0; j < 3; j++){
+        if(this.trailCheck(0, j, 1, 0, playerMark[i]))
+          return i+1;
+      }
+
+      //Check diagonal
+      if(this.trailCheck(0, 0, 1, 1, playerMark[i]))
+        return i+1;
+
+      //Check other diagonal
+      if(this.trailCheck(0, 2, 1, -1, playerMark[i]))
+        return i+1;
+    }
+    return 0;
+  }
+
+  this.trailCheck = function(row, col, rDirect, cDirect, playerMark){
+    if(this.moves[row][col] == playerMark)
+      if(this.moves[row][col] == this.moves[row+rDirect][col+cDirect] && this.moves[row][col] == this.moves[row+2*rDirect][col+2*cDirect])
+        return true;
+
+    return false;
+  }
+
+  this.updatePlayPoints();
+}
